af02f796d9b19386b37eb5e7d59c32e58a157d55
jeffrepo <jeffer.leo93@gmail.com>
2022-10-31 21:43:51 -0600

    update

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Technotrade",
+
+    'summary': """ POS Technotrade """,
+
+    'description': """
+        Sincronización con dispositivo para gasolineras usando el controlador Technotrade
+    """,
+
+    'author': "STECHNOLOGIES",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['point_of_sale'],
+
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
