fbdd6e9cb197a92b6af7fbfff3097d98dcac834e
jeffrepo <jeffer.leo93@gmail.com>
2022-12-13 18:13:27 -0600

    update

--- controller/http.py
+++ controller/http.py
@@ -15,21 +15,6 @@ _logger = logging.getLogger(__name__)
 
 class JsonRPCDispatcherPatch(JsonRPCDispatcher):
 
-    # def _response(self, result=None, error=None):
-    #     request_id = self.jsonrequest.get('id')
-    #     response = {'jsonrpc': '1.0', 'id': request_id}
-    #
-    #     if error is not None:
-    #         response['error'] = error
-    #     if result is not None:
-    #         response['result'] = result
-    #         if "Packets" in result:
-    #             logging.warning('response ibriman')
-    #             response = result
-    #             logging.warning(response)
-    #
-    #     return self.request.make_json_response(response)
-
     def _response(self, result=None, error=None):
         res = super(JsonRPCDispatcherPatch, self)._response(result,error)
         logging.warning('res response')
